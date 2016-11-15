d5f242f2114f263a303de661b1dc7e17b340309a
Margo Sokolova <ritaritarita2013@gmail.com>
2016-11-15 04:48:31 +0300

    fix():JSON parsing

--- src/main/java/com/main/map/models/AreaInformation.java
+++ src/main/java/com/main/map/models/AreaInformation.java
@@ -6,6 +6,7 @@ import com.main.getOpenData.DAO.Company;
 import com.main.getOpenData.DAO.CompanyDao;
 import com.main.getOpenData.Point;
 import com.main.map.models.JSONclasses.AreaQuery;
+import com.main.map.models.JSONclasses.EstimatedArea;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,126 +17,43 @@ import java.util.Iterator;
 import java.util.List;
 
 public class AreaInformation {
-    private final int TYPES_NUMBER = 7;
-    private double[] importances;
-    private double[] coordinates;
-    private double[] coordinatesRadius;
-    private int[] typeIN;
     private double estimate;
+    private AreaQuery areaQuery;
 
     public void requestHandling(String jsonQueryStr, CompanyDao companyDao) {
-        System.out.println("1");
-        parsingJsonQueryStr(jsonQueryStr);
-        //calculateEstimate();
+        this.areaQuery = parsingJsonQueryStr(jsonQueryStr);
+        this.estimate = calculateEstimate();
         //return createAnswerJson(companyDao);
     }
 
-    public void parsingJsonQueryStr(String jsonQueryStr) {
-        System.out.println("Start serialization");
-        System.out.println(jsonQueryStr);
+    public AreaQuery parsingJsonQueryStr(String jsonQueryStr) {
         Gson gson = new GsonBuilder().create();
         AreaQuery object = gson.fromJson(jsonQueryStr, AreaQuery.class);
+        /*
         System.out.println(object.getCoordinates()[0]+":"+object.getCoordinates()[1]);
         System.out.println(object.getDistrict());
         System.out.println(object.getRadius());
         System.out.println(object.getNorthPoint()[0]+":"+object.getNorthPoint()[1]);
         for (int i = 0; i < object.getEstimateParams().size(); i++) {
-            System.out.println(object.getEstimateParam(i));
+            System.out.println(object.getEstimateParam(i).getImportance());
+            System.out.println(object.getEstimateParam(i).getType());
         }
-        System.out.println("Stop serialization");
+        */
+        return object;
     }
 
-    private void calculateEstimate() {
-        System.out.println("2");
-        double result = 0;
-        for (double x : importances) {
-            result += x;
-        }
-        result /= importances.length;
-        result = Math.round(result*100);
-        estimate = result/100;
+    private int calculateEstimate() {
+       /*надо придумать */
+       int estimate = 5;
+       return estimate;
     }
-
+/*
     private String createAnswerJson(CompanyDao companyDao) {
-        System.out.println("3");
-        JsonObject answerRootObject = new JsonObject();
-        answerRootObject.add("estimate", new JsonPrimitive(estimate));
 
-        JsonObject districtRating = new JsonObject();
-        districtRating.add("safety", new JsonPrimitive(1));
-        districtRating.add("life_quality", new JsonPrimitive(1));
-        districtRating.add("transport_quality", new JsonPrimitive(1));
-        districtRating.add("rest_availability", new JsonPrimitive(1));
-        districtRating.add("parks_availability", new JsonPrimitive(1));
-        answerRootObject.add("district-rating", districtRating);
 
-        JsonArray arrayMetro = new JsonArray();
-        JsonObject metro = new JsonObject();
-        metro.add("name", new JsonPrimitive("Невский проспект"));
-        metro.add("distance", new JsonPrimitive("1.5 км"));
-        metro.add("color", new JsonPrimitive(1));
-        arrayMetro.add(metro);
-        JsonObject metro2 = new JsonObject();
-        metro2.add("name", new JsonPrimitive("Парк победы"));
-        metro2.add("distance", new JsonPrimitive("4.8 км"));
-        metro2.add("color", new JsonPrimitive(2));
-        arrayMetro.add(metro2);
-        answerRootObject.add("metro", arrayMetro);
-
-//        JsonObject geoObject = parseDataForGeoObject(getYandexGeocodeJSON(coordinates));
-//        double[] point = getPoint(geoObject);   //положение(координаты) найденного объекта, по версии Yandex
-//
-//        JsonObject target = new JsonObject();
-//        JsonPrimitive address = new JsonPrimitive(getAddress(geoObject));
-//        target.add("address", address);
-
-//        JsonArray coordinatesJson = new JsonArray();
-//        coordinatesJson.add(point[0]);
-//        coordinatesJson.add(point[1]);
-//        target.add("coordinates", coordinatesJson);
-//
-//        answerRootObject.add("target", target);
-
-        JsonArray infrastructure = new JsonArray(); // для ответа - массив объектов в радиусе
-
-        if (typeIN != null) {
-            //int[] typeIN = {1,2,3,4,5,6,7};
-            Point p0 = new Point(coordinates[1], coordinates[0]), pRad = new Point(coordinatesRadius[1], coordinatesRadius[0]);
-            List<Company> list = getInRadius(p0, pRad, typeIN, companyDao);
-
-            JsonObject company;
-            for (Company com : list) {
-                company = new JsonObject();
-                JsonPrimitive companyAddress = new JsonPrimitive(com.getAddress());
-                company.add("address", companyAddress);
-                JsonPrimitive companyName = new JsonPrimitive(com.getName());
-                company.add("name", companyName);
-                JsonPrimitive companyType = new JsonPrimitive(com.getIdType());
-                company.add("type", companyType);
-                JsonArray companyCoordinates = new JsonArray();//coordinates
-                companyCoordinates.add(com.getLatitude());
-                companyCoordinates.add(com.getLongitude());
-                company.add("coordinates", companyCoordinates);
-
-                infrastructure.add(company);
-            }
-        }
-        answerRootObject.add("infrastructure", infrastructure);
-
-        System.out.println("jSON ответ"+answerRootObject.toString()); //ответ готов
-        return answerRootObject.toString();
+        return ;
     }
-
-    private String getAddress(JsonObject geoObject) {
-        return geoObject.getAsJsonPrimitive("name").getAsString();
-    }
-
-    private double[] getPoint(JsonObject geoObject) {
-        String[] coord = geoObject.getAsJsonObject("Point").getAsJsonPrimitive("pos").getAsString().split(" ");
-        double c[] = {Double.parseDouble(coord[1]), Double.parseDouble(coord[0])};
-        return c;
-    }
-
+*/
 
     private String getYandexGeocodeJSON(double[] coordinates) {
         URL url;
@@ -160,24 +78,6 @@ public class AreaInformation {
     }
 
 
-    private JsonObject parseDataForGeoObject(String strJson) {
-        JsonObject rootObject = (new JsonParser()).parse(strJson).getAsJsonObject(); // чтение главного объекта
-//        return rootObject.getAsJsonObject("response")
-//                .getAsJsonObject("GeoObjectCollection")
-//                .getAsJsonArray("featureMember").get(0).getAsJsonObject()
-//                .getAsJsonObject("GeoObject")
-//                .getAsJsonObject("metaDataProperty")
-//                .getAsJsonObject("GeocoderMetaData")
-//                .getAsJsonObject("AddressDetails")
-//                .getAsJsonObject("Country")
-//                .getAsJsonPrimitive("AddressLine").getAsString();
-        return rootObject.getAsJsonObject("response")
-                .getAsJsonObject("GeoObjectCollection")
-                .getAsJsonArray("featureMember").get(0).getAsJsonObject()
-                .getAsJsonObject("GeoObject");
-    }
-
-
     private List<Company> getInRadius(Point centreCoor, Point upCoor, int[] typeIN, CompanyDao companyDao) {
         double radius = upCoor.getY() - centreCoor.getY();
         double radiusSquared = Math.pow(radius, 2);
@@ -198,6 +98,5 @@ public class AreaInformation {
         // listCompany.forEach(item -> System.out.println(item.getName()));
         return listCompany;
     }
-
 }
 
