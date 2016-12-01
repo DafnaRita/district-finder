4eb71d9ab2329edaef3246fae95758e509d391bc
Margo Sokolova <ritaritarita2013@gmail.com>
2016-12-01 19:30:42 +0300

    fix(): fix sign in admin console && fix basic autentification

--- src/main/java/com/main/auth/controllers/LoginController.java
+++ src/main/java/com/main/auth/controllers/LoginController.java
@@ -1,12 +1,17 @@
 package com.main.auth.controllers;
 
+import com.google.gson.Gson;
 import com.main.auth.DAO.UserHebirnate;
+import com.main.auth.model.JSONclasses.AdminAnswer;
+import com.main.auth.model.JSONclasses.AuthAnswer;
 import com.main.auth.model.UserHandler;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,45 +24,26 @@ import java.security.Principal;
 
 @RestController
 public class LoginController {
-    @Autowired
-    private UserDao userDao;
 
     private Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    //войти на карту
-    @RequestMapping(value = "/auth", method = RequestMethod.POST,
-            produces = "application/json")
-    public String auth(@RequestBody String jsonQueryStr) {
-        System.out.println("/auth");
-        UserHandler handler = new UserHandler(userDao);
-        handler.parseJSON(jsonQueryStr);
-        System.out.println("готовимся в checkUser():");
-        System.out.println("handler.checkUser()"+handler.checkUser());
-        return handler.checkUser();
-    }
-
     @RequestMapping(value = "/login", method = RequestMethod.GET,
             produces = "application/json")
-    public UserHebirnate getUser(Principal principial) {
-
-        if (principial != null) {
-
-            log.info("Got user info for login = " + principial.getName());
-
-            if (principial instanceof AbstractAuthenticationToken){
-                return (UserHebirnate) ((AbstractAuthenticationToken)
-                        principial).getPrincipal();
-            }
-        }
-        return null;
+    public String auth() {
+        Gson gson = new Gson();
+        AuthAnswer request = new AuthAnswer();
+        request.setAuth(true);
+        request.setError("none");
+        return gson.toJson(request);
     }
 
-    @RequestMapping(value = "/logout", method = RequestMethod.POST)
-    public void logout(HttpServletRequest rq, HttpServletResponse rs) {
-
-        SecurityContextLogoutHandler securityContextLogoutHandler =
-                new SecurityContextLogoutHandler();
-        securityContextLogoutHandler.logout(rq, rs, null);
+    @RequestMapping(value = "/admin/refreshData", method = RequestMethod.GET,
+            produces = "application/json")
+    public String refreshData() {
+        /*Вставить логику для обновления данных*/
+        Gson gson = new Gson();
+        AdminAnswer request = new AdminAnswer();
+        request.setRefreshed(true);
+        return gson.toJson(request);
     }
-
 }
--- src/main/java/com/main/auth/model/UserHandler.java
+++ src/main/java/com/main/auth/model/UserHandler.java
@@ -7,49 +7,14 @@ import com.main.auth.DAO.UserHebirnate;
 import com.main.auth.model.JSONclasses.AuthAnswer;
 import com.main.auth.model.JSONclasses.AuthQuery;
 
-/**
- * Created by Margo on 29.11.2016.
- */
 public class UserHandler {
-
-    UserHebirnate userHebirnate;
     Gson gson;
-    String queryLog;
-    String queryPass;
-    UserDao userDao;
-
-    public UserHandler(UserDao user) {
-        userDao = user;
-    }
 
     public void parseJSON(String jsonQueryStr){
-        System.out.println("parseJSON:");
         gson = new GsonBuilder().create();
-        AuthQuery object = gson.fromJson(jsonQueryStr, AuthQuery.class);
-        queryLog = object.getLogin();
-        queryPass = object.getPass();
-        System.out.println("распарсеный логин:"+queryLog);
-        System.out.println("распарсеный пароль:"+queryPass);
     }
 
     public String checkUser(){
-        System.out.println("checkUser!:"+queryLog);
-        System.out.println("userDao!:"+userDao);
-        System.out.println("userDao.findByLogin(queryLog)!:"+userDao.findByLogin(queryLog));
-        if(userDao.findByLogin(queryLog) == null){
-            System.out.println("ошибка: нет такого пользователя");
-            return gson.toJson(new AuthAnswer(false, "notExist"));
-        } else {
-            System.out.println("такой пользователь есть");
-            userHebirnate = userDao.findByLogin(queryLog);
-        }
-        if(!userHebirnate.getPassword().equals(queryPass)) {
-            System.out.println("userHebirnate.getPassword():"+userHebirnate.getPassword());
-            System.out.println("queryPass:"+queryPass);
-            System.out.println("ошибка: неправильный пароль");
-            return gson.toJson(new AuthAnswer(false, "wrongPass"));
-        }
-        System.out.println("Пользователь валиден");
-        return gson.toJson(new AuthAnswer(true,"none"));
+       return "lal";
     }
 }
